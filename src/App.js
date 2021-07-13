21f8792db400505e290e1d543cb258bef6258485
Peter Mei <Peter.Mei@am.jll.com>
2021-07-13 14:23:45 -0700

    Create Modal Dialog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+
+
+import React, { useState} from 'react';
+import Modal from 'react-modal';
 import logo from './logo.svg';
 import './App.css';
 
+Modal.setAppElement('#root')
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
+      <Modal 
+        isOpen={modalIsOpen} 
+        shouldCloseOnOverlayClick={true} 
+        onRequestClose={() => setModalIsOpen(false)} 
+        style={
+          {
+            overlay: {
+              backgroundColor: 'grey'
+            },
+            content: {
+              color:'orange'
+            }
+          }
+        }>
+        <h2>Modal Title</h2>
+        <p>Modal Body</p>
+        <div>
+          <button onClick={() => setModalIsOpen(false)}>Close</button>
+        </div>
+      </Modal>
     </div>
   );
 }
